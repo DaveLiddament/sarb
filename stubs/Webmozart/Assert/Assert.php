516c5c1e89a72d0ae993c1618b02079becf7fd93
Dave Liddament <daveliddament@gmail.com>
2018-11-02 19:57:30 +0000

    UPDATE min version of psalm and remove dirty hack

--- stubs/Webmozart/Assert/Assert.php
+++ stubs/Webmozart/Assert/Assert.php
@@ -20,10 +20,7 @@ class Assert
      * @template T
      * @template-typeof T $type
      * @param class-string $type
-     *
-     * TODO massive hack. Replace FQCN of GitCommit to T when a new version of psalm is ready that includes this fix: https://github.com/vimeo/psalm/issues/1044
-     *
-     * @psalm-assert DaveLiddament\StaticAnalysisBaseliner\Plugins\GitDiffHistoryAnalyser\GitCommit $value
+     * @psalm-assert T $value
      */
     public static function isInstanceOf($value, $type, $message = '') {}
 }
