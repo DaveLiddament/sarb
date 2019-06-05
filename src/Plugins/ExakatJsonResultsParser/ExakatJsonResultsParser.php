e2b51d4b852add06cf3b91506c638ce924782e0e
Damien Seguy <damien.seguy@gmail.com>
2019-06-05 17:49:46 +0200

    Initial commit for Exakat support

---
+++ src/Plugins/ExakatJsonResultsParser/ExakatJsonResultsParser.php
@@ -0,0 +1,145 @@
+<?php
+
+/**
+ * Static Analysis Results Baseliner (sarb).
+ *
+ * (c) Dave Liddament
+ *
+ * For the full copyright and licence information please view the LICENSE file distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace DaveLiddament\StaticAnalysisResultsBaseliner\Plugins\ExakatJsonResultsParser;
+
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\FileName;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\InvalidPathException;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\LineNumber;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\Location;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\ProjectRoot;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\Type;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\File\InvalidFileFormatException;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\ResultsParser\AnalysisResult;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\ResultsParser\AnalysisResults;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\ResultsParser\Identifier;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\ResultsParser\ResultsParser;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Utils\ArrayParseException;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Utils\ArrayUtils;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Utils\JsonParseException;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Utils\JsonUtils;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Utils\ParseAtLocationException;
+
+class ExakatJsonResultsParser implements ResultsParser
+{
+    const LINE_FROM = 'line';
+    const TYPE = 'type';
+    const FILE = 'file';
+// Not used yet 
+//    const SEVERITY = 'severity';
+//    const ERROR_SEVERITY_LEVEL = 'error';
+//    const MESSAGE = 'message';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertFromString(string $resultsAsString, ProjectRoot $projectRoot): AnalysisResults
+    {
+        try {
+            $asArray = JsonUtils::toArray($resultsAsString);
+        } catch (JsonParseException $e) {
+            throw new InvalidFileFormatException('Not a valid JSON format');
+        }
+
+        return $this->convertFromArray($asArray, $projectRoot);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToString(AnalysisResults $analysisResults): string
+    {
+        $asArray = [];
+        foreach ($analysisResults->getAnalysisResults() as $analysisResult) {
+            $asArray[] = JsonUtils::toArray($analysisResult->getFullDetails());
+        }
+
+        return JsonUtils::toString($asArray);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdentifier(): Identifier
+    {
+        return new ExakatJsonIdentifier();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function showTypeGuessingWarning(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Converts from an array.
+     *
+     * @param array $analysisResultsAsArray
+     * @param ProjectRoot $projectRoot
+     *
+     * @throws ParseAtLocationException
+     *
+     * @return AnalysisResults
+     */
+    private function convertFromArray(array $analysisResultsAsArray, ProjectRoot $projectRoot): AnalysisResults
+    {
+        $analysisResults = new AnalysisResults();
+
+        $resultsCount = 0;
+
+        foreach ($analysisResultsAsArray as $analysisResultAsArray) {
+            ++$resultsCount;
+            try {
+                ArrayUtils::assertArray($analysisResultAsArray);
+                $analysisResult = $this->convertAnalysisResultFromArray($analysisResultAsArray, $projectRoot);
+                $analysisResults->addAnalysisResult($analysisResult);
+            } catch (ArrayParseException | JsonParseException | InvalidPathException $e) {
+                throw new ParseAtLocationException("Result [$resultsCount]", $e);
+            }
+        }
+
+        return $analysisResults;
+    }
+
+    /**
+     * @param array $analysisResultAsArray
+     *
+     * @throws ArrayParseException
+     * @throws JsonParseException
+     * @throws InvalidPathException
+     *
+     * @return AnalysisResult
+     */
+    private function convertAnalysisResultFromArray(
+        array $analysisResultAsArray,
+        ProjectRoot $projectRoot
+    ): AnalysisResult {
+        $absoluteFileNameAsString = ArrayUtils::getStringValue($analysisResultAsArray, self::FILE);
+        $lineAsInt = ArrayUtils::getIntValue($analysisResultAsArray, self::LINE_FROM);
+        $typeAsString = ArrayUtils::getStringValue($analysisResultAsArray, self::TYPE);
+        $relativeFileNameAsString = $projectRoot->getPathRelativeToRootDirectory($absoluteFileNameAsString);
+        
+        $location = new Location(
+            new FileName($relativeFileNameAsString),
+            new LineNumber($lineAsInt)
+        );
+
+        return new AnalysisResult(
+            $location,
+            new Type($typeAsString),
+            '',
+            JsonUtils::toString($analysisResultAsArray)
+        );
+    }
+}
