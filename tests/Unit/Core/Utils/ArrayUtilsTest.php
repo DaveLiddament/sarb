c248d41c6351a8d57bf7baf4a7f86e9f20e9eb3e
Dave Liddament <dave@lampbristol.com>
2020-08-11 10:10:47 +0000

    ADD missing test coverage for ArrayUtils

--- tests/Unit/Core/Utils/ArrayUtilsTest.php
+++ tests/Unit/Core/Utils/ArrayUtilsTest.php
@@ -15,8 +15,10 @@ class ArrayUtilsTest extends TestCase
         self::AGE_KEY => self::AGE_VALUE,
         self::ADDRESS_KEY => self::ADDRESS_VALUE,
         self::INT_AS_STRING_KEY => self::INT_AS_STRING_VALUE,
+        self::NULL_KEY => null,
     ];
 
+    private const NULL_KEY = 'null';
     private const NAME_KEY = 'name';
     private const AGE_KEY = 'age';
     private const ADDRESS_KEY = 'address';
@@ -70,5 +72,49 @@ class ArrayUtilsTest extends TestCase
     {
         $this->expectException(ArrayParseException::class);
         ArrayUtils::getArrayValue(self::TEST_ARRAY, self::AGE_KEY);
+    }
+
+    public function testGetIntOrNullWithInt(): void
+    {
+        $actual = ArrayUtils::getIntOrNullValue(self::TEST_ARRAY, self::AGE_KEY);
+        $this->assertEquals(self::AGE_VALUE, $actual);
+    }
+
+    public function testGetIntOrNullWithNull(): void
+    {
+        $actual = ArrayUtils::getIntOrNullValue(self::TEST_ARRAY, self::NULL_KEY);
+        $this->assertNull($actual);
+    }
+
+    public function testGetIntOrNullWithString(): void
+    {
+        $this->expectException(ArrayParseException::class);
+        ArrayUtils::getIntOrNullValue(self::TEST_ARRAY, self::NAME_KEY);
+    }
+
+    public function testGetInasAsStringNoneString(): void
+    {
+        $this->expectException(ArrayParseException::class);
+        ArrayUtils::getIntAsStringValue(self::TEST_ARRAY, self::AGE_KEY);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testAssertArrayWithArray(): void
+    {
+        ArrayUtils::assertArray(self::TEST_ARRAY);
+    }
+
+    public function testAssertArratOnNoneArray(): void
+    {
+        $this->expectException(ArrayParseException::class);
+        ArrayUtils::assertArray(self::AGE_VALUE);
+    }
+
+    public function testInvalidArrayKey(): void
+    {
+        $this->expectException(ArrayParseException::class);
+        ArrayUtils::getStringValue(self::TEST_ARRAY, 'foo');
     }
 }
