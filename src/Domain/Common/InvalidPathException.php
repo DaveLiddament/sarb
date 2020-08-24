f6e9ca0f4687340ed8656e7febf730b91bbbb12d
Dave Liddament <dave@lampbristol.com>
2020-08-24 10:41:05 +0000

    FIX CS

--- src/Domain/Common/InvalidPathException.php
+++ src/Domain/Common/InvalidPathException.php
@@ -6,7 +6,7 @@ namespace DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common;
 
 class InvalidPathException extends SarbException
 {
-    public static function newInstance(string  $path, string $projectRootBasePath): self
+    public static function newInstance(string $path, string $projectRootBasePath): self
     {
         $message = "Path [$path] not in the project root [$projectRootBasePath]. Is project root configured correctly?";
 
