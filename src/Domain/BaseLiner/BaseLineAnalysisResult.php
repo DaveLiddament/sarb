46010466fed26e6b656258c87418899d8009c0cf
Dave Liddament <dave@lampbristol.com>
2020-08-25 14:16:36 +0000

    ADD objects to hold single BaseLineAnalysisResult

---
+++ src/Domain/BaseLiner/BaseLineAnalysisResult.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaveLiddament\StaticAnalysisResultsBaseliner\Domain\BaseLiner;
+
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\FileName;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\LineNumber;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\Location;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\Type;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Utils\ArrayParseException;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Utils\ArrayUtils;
+
+class BaseLineAnalysisResult
+{
+    private const LINE_NUMBER = 'lineNumber';
+    private const FILE_NAME = 'fileName';
+    private const TYPE = 'type';
+    private const MESSAGE = 'message';
+
+    /**
+     * @var FileName
+     */
+    private $fileName;
+    /**
+     * @var LineNumber
+     */
+    private $lineNumber;
+    /**
+     * @var Type
+     */
+    private $type;
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @phpstan-param array<mixed> $array
+     *
+     * @throws ArrayParseException
+     */
+    public static function fromArray(array $array): self
+    {
+        $lineNumber = new LineNumber(ArrayUtils::getIntValue($array, self::LINE_NUMBER));
+        $fileName = new FileName(ArrayUtils::getStringValue($array, self::FILE_NAME));
+        $type = new Type(ArrayUtils::getStringValue($array, self::TYPE));
+        $message = ArrayUtils::getStringValue($array, self::MESSAGE);
+
+        return new self($fileName, $lineNumber, $type, $message);
+    }
+
+    public static function make(
+        FileName $fileName,
+        LineNumber $lineNumber,
+        Type $type,
+        string $message
+    ): self {
+        return new self($fileName, $lineNumber, $type, $message);
+    }
+
+    private function __construct(FileName $fileName, LineNumber $lineNumber, Type $type, string $message)
+    {
+        $this->fileName = $fileName;
+        $this->lineNumber = $lineNumber;
+        $this->type = $type;
+        $this->message = $message;
+    }
+
+    public function getFileName(): FileName
+    {
+        return $this->fileName;
+    }
+
+    public function getLineNumber(): LineNumber
+    {
+        return $this->lineNumber;
+    }
+
+    public function getType(): Type
+    {
+        return $this->type;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @phpstan-return array<string,string|int>
+     */
+    public function asArray(): array
+    {
+        return [
+            self::LINE_NUMBER => $this->getLineNumber()->getLineNumber(),
+            self::FILE_NAME => $this->getFileName()->getFileName(),
+            self::TYPE => $this->type->getType(),
+            self::MESSAGE => $this->message,
+        ];
+    }
+
+    /**
+     * Return true if this matches matches given FileName, LineNumber and type.
+     */
+    public function isMatch(Location $location, Type $type): bool
+    {
+        return
+            $this->fileName->isEqual($location->getFileName()) &&
+            $this->lineNumber->isEqual($location->getLineNumber()) &&
+            $this->type->isEqual($type);
+    }
+}
---
+++ tests/Unit/Core/BaseLiner/BaseLineAnalysisResultTest.php
@@ -0,0 +1,158 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaveLiddament\StaticAnalysisResultsBaseliner\Tests\Unit\Core\BaseLiner;
+
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\BaseLiner\BaseLineAnalysisResult;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\FileName;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\LineNumber;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\Location;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\Type;
+use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Utils\ArrayParseException;
+use PHPUnit\Framework\TestCase;
+
+class BaseLineAnalysisResultTest extends TestCase
+{
+    private const FILE_NAME_1 = 'fileName1';
+    private const FILE_NAME_2 = 'fileName2';
+    private const LINE_NUMBER_1 = 1;
+    private const LINE_NUMBER_2 = 2;
+    private const TYPE_1 = 'TYPE_1';
+    private const TYPE_2 = 'TYPE_2';
+    private const MESSAGE_1 = 'MESSAGE_1';
+
+    /**
+     * @var FileName
+     */
+    private $fileName;
+
+    /**
+     * @var LineNumber
+     */
+    private $lineNumber;
+
+    /**
+     * @var Type
+     */
+    private $type;
+
+    protected function setUp(): void
+    {
+        $this->fileName = new FileName(self::FILE_NAME_1);
+        $this->lineNumber = new LineNumber(self::LINE_NUMBER_1);
+        $this->type = new Type(self::TYPE_1);
+    }
+
+    public function testBuild(): void
+    {
+        $baseLineAnalysisResult = $this->createBaseLineResult();
+
+        $this->assertSame($this->fileName, $baseLineAnalysisResult->getFileName());
+        $this->assertSame($this->lineNumber, $baseLineAnalysisResult->getLineNumber());
+        $this->assertSame($this->type, $baseLineAnalysisResult->getType());
+        $this->assertSame(self::MESSAGE_1, $baseLineAnalysisResult->getMessage());
+    }
+
+    public function testConvertToAndFromArray(): void
+    {
+        $originalResult = $this->createBaseLineResult();
+
+        $asArray = $originalResult->asArray();
+        $unserialisedBaseLineAnalysisResult = BaseLineAnalysisResult::fromArray($asArray);
+
+        // Should not be identical objects
+        $this->assertNotSame($this->fileName, $unserialisedBaseLineAnalysisResult->getFileName());
+        $this->assertNotSame($this->lineNumber, $unserialisedBaseLineAnalysisResult->getLineNumber());
+        $this->assertNotSame($this->type, $unserialisedBaseLineAnalysisResult->getType());
+
+        // Values should be the same though
+        $this->assertTrue($this->fileName->isEqual($unserialisedBaseLineAnalysisResult->getFileName()));
+        $this->assertTrue($this->lineNumber->isEqual($unserialisedBaseLineAnalysisResult->getLineNumber()));
+        $this->assertTrue($this->type->isEqual($unserialisedBaseLineAnalysisResult->getType()));
+        $this->assertSame(self::MESSAGE_1, $unserialisedBaseLineAnalysisResult->getMessage());
+    }
+
+    public function testIsActualMatch(): void
+    {
+        $baseLineAnalysisResult = $this->createBaseLineResult();
+        $location = new Location($this->fileName, $this->lineNumber);
+        $this->assertTrue($baseLineAnalysisResult->isMatch($location, new Type(self::TYPE_1)));
+    }
+
+    public function testIsMatchDifferentType(): void
+    {
+        $baseLineAnalysisResult = $this->createBaseLineResult();
+        $location = new Location($this->fileName, $this->lineNumber);
+        $this->assertFalse($baseLineAnalysisResult->isMatch($location, new Type(self::TYPE_2)));
+    }
+
+    public function testIsMatchDifferentFileName(): void
+    {
+        $baseLineAnalysisResult = $this->createBaseLineResult();
+        $location = new Location(new FileName(self::FILE_NAME_2), $this->lineNumber);
+        $this->assertFalse($baseLineAnalysisResult->isMatch($location, new Type(self::TYPE_1)));
+    }
+
+    public function testIsMatchDifferentLineNumber(): void
+    {
+        $baseLineAnalysisResult = $this->createBaseLineResult();
+        $location = new Location($this->fileName, new LineNumber(self::LINE_NUMBER_2));
+        $this->assertFalse($baseLineAnalysisResult->isMatch($location, new Type(self::TYPE_1)));
+    }
+
+    public function invalidArrayDataProvider(): array
+    {
+        return [
+            'missingLineNumber' => [
+                [
+                    'fileName' => self::FILE_NAME_1,
+                    'type' => self::TYPE_1,
+                    'message' => self::MESSAGE_1,
+                ],
+            ],
+            'missingFileName' => [
+                [
+                    'lineNumber' => self::LINE_NUMBER_1,
+                    'type' => self::TYPE_1,
+                    'message' => self::MESSAGE_1,
+                ],
+            ],
+            'missingType' => [
+                [
+                    'lineNumber' => self::LINE_NUMBER_1,
+                    'fileName' => self::FILE_NAME_1,
+                    'message' => self::MESSAGE_1,
+                ],
+            ],
+            [
+                'missingMessage' => [
+                    'lineNumber' => self::LINE_NUMBER_1,
+                    'fileName' => self::FILE_NAME_1,
+                    'type' => self::TYPE_1,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidArrayDataProvider
+     */
+    public function testInvalidArray(array $asArray): void
+    {
+        $this->expectException(ArrayParseException::class);
+        BaseLineAnalysisResult::fromArray($asArray);
+    }
+
+    private function createBaseLineResult(): BaseLineAnalysisResult
+    {
+        $baseLineAnalysisResult = BaseLineAnalysisResult::make(
+            $this->fileName,
+            $this->lineNumber,
+            $this->type,
+            self::MESSAGE_1
+        );
+
+        return $baseLineAnalysisResult;
+    }
+}
