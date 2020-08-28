c325ef6a46905685053262c2b064e04aa054eaaf
Dave Liddament <dave@lampbristol.com>
2020-08-28 19:12:30 +0000

    REMOVE deprecated methods

--- src/Domain/ResultsParser/AnalysisResults.php
+++ src/Domain/ResultsParser/AnalysisResults.php
@@ -12,9 +12,6 @@ declare(strict_types=1);
 
 namespace DaveLiddament\StaticAnalysisResultsBaseliner\Domain\ResultsParser;
 
-use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Utils\ArrayParseException;
-use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Utils\ArrayUtils;
-
 /**
  * Holds all results from a run of the static analysis results.
  */
@@ -58,47 +55,6 @@ class AnalysisResults
         });
 
         return $this->analysisResults;
-    }
-
-    /**
-     * Return as an array of arrays (ready for storing in a file).
-     *
-     * @phpstan-return array<mixed>
-     *
-     * @deprecated
-     */
-    public function asArray(): array
-    {
-        $array = [];
-        foreach ($this->getAnalysisResults() as $analysisResult) {
-            $array[] = $analysisResult->asArray();
-        }
-
-        return $array;
-    }
-
-    /**
-     * Deserialises array representation to AnalysisResults.
-     *
-     * @throws ArrayParseException
-     * @phpstan-param array<mixed> $array
-     *
-     * @return AnalysisResults
-     *
-     * @deprecated
-     */
-    public static function fromArray(array $array): self
-    {
-        $analysisResults = new self();
-
-        /** @psalm-suppress MixedAssignment */
-        foreach ($array as $entry) {
-            ArrayUtils::assertArray($entry);
-            $analysisResult = AnalysisResult::fromArray($entry);
-            $analysisResults->addAnalysisResult($analysisResult);
-        }
-
-        return $analysisResults;
     }
 
     public function getCount(): int
