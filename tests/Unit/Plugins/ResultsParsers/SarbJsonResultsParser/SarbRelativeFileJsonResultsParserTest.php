03063c76b5745a0ca73348a874b6a958880f3be1
Dave Liddament <dave@lampbristol.com>
2021-08-28 13:08:05 +0000

    FIX type hint

--- tests/Unit/Plugins/ResultsParsers/SarbJsonResultsParser/SarbRelativeFileJsonResultsParserTest.php
+++ tests/Unit/Plugins/ResultsParsers/SarbJsonResultsParser/SarbRelativeFileJsonResultsParserTest.php
@@ -7,7 +7,6 @@ namespace DaveLiddament\StaticAnalysisResultsBaseliner\Tests\Unit\Plugins\Result
 use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common\ProjectRoot;
 use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\File\InvalidContentTypeException;
 use DaveLiddament\StaticAnalysisResultsBaseliner\Domain\ResultsParser\AnalysisResults;
-use DaveLiddament\StaticAnalysisResultsBaseliner\Plugins\ResultsParsers\SarbJsonResultsParser\SarbJsonResultsParser;
 use DaveLiddament\StaticAnalysisResultsBaseliner\Plugins\ResultsParsers\SarbJsonResultsParser\SarbRelativeFileJsonResultsParser;
 use DaveLiddament\StaticAnalysisResultsBaseliner\Tests\Helpers\AssertFileContentsSameTrait;
 use DaveLiddament\StaticAnalysisResultsBaseliner\Tests\Helpers\AssertResultMatch;
@@ -28,7 +27,7 @@ class SarbRelativeFileJsonResultsParserTest extends TestCase
     private $analysisResults;
 
     /**
-     * @var SarbJsonResultsParser
+     * @var SarbRelativeFileJsonResultsParser
      */
     private $sarbRelativeFileJsonResultsParser;
     /**
