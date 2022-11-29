2d81824fb96e3c94d38ca5eb80c691e04673c119
Dave Liddament <dave@lampbristol.com>
2022-11-29 19:31:18 +0000

    ADD Severity value object

---
+++ src/Domain/Common/Severity.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaveLiddament\StaticAnalysisResultsBaseliner\Domain\Common;
+
+use Webmozart\Assert\Assert;
+
+class Severity
+{
+    public const WARNING = 'warning';
+    public const ERROR = 'error';
+
+    /**
+     * @var string
+     */
+    private $severity;
+
+    public static function fromStringOrNull(?string $severity): self
+    {
+        if (null === $severity) {
+            return new self(self::ERROR);
+        }
+
+        return new self($severity);
+    }
+
+    public static function error(): self
+    {
+        return new self(self::ERROR);
+    }
+
+    private function __construct(string $severity)
+    {
+        Assert::true(self::isValueValid($severity), "Invalid severity: $severity");
+        $this->severity = $severity;
+    }
+
+    public function getSeverity(): string
+    {
+        return $this->severity;
+    }
+
+    public static function isValueValid(string $severity): bool
+    {
+        return in_array($severity, [self::ERROR, self::WARNING], true);
+    }
+}
